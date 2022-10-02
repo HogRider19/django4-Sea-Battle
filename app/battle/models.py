73f6309b3f66c608912849e0ae0fef5489caaaa2
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-10-03 01:19:51 +0300

    Added userprofile!

--- app/battle/models.py
+++ app/battle/models.py
@@ -6,16 +6,17 @@ class Game(models.Model):
     """Модель игры"""
     name = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=10)
+    battle_map = models.CharField(max_length=500)
+    winner = models.OneToOneField(User, on_delete=models.DO_NOTHING)
     player1 = models.OneToOneField(
-        User, related_name='game', on_delate=models.DO_NOTHING)
+        User, related_name='game', on_delete=models.DO_NOTHING)
     player2 = models.OneToOneField(
-        User, related_name='game', on_delate=models.DO_NOTHING)
-    spectators = models.ManyToManyField(
-        User, related_name='viewed_game', on_delate=models.DO_NOTHING())
-    subscribers = models.ManyToManyField(
-        User, related_name='viewed_game', on_delate=models.DO_NOTHING())
+        User, related_name='game', on_delete=models.DO_NOTHING)
+    spectators = models.ManyToManyField(User, related_name='viewed_game')
+    subscribers = models.ManyToManyField(User, related_name='viewed_game')
     create_at = models.DateTimeField(auto_now_add=True)
     start_at = models.DateTimeField()
+    finished_at = models.DecimalField()
 
     def __str__(self) -> str:
         return self.name
