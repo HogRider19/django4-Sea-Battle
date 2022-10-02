1e561953ab466fe6ba5cadec44c7a2e73455b347
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-10-03 01:00:09 +0300

    Added Game model!

--- app/battle/models.py
+++ app/battle/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Game(models.Model):
+    """Модель игры"""
+    name = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=10)
+    player1 = models.OneToOneField(
+        User, related_name='game', on_delate=models.DO_NOTHING)
+    player2 = models.OneToOneField(
+        User, related_name='game', on_delate=models.DO_NOTHING)
+    spectators = models.ManyToManyField(
+        User, related_name='viewed_game', on_delate=models.DO_NOTHING())
+    subscribers = models.ManyToManyField(
+        User, related_name='viewed_game', on_delate=models.DO_NOTHING())
+    create_at = models.DateTimeField(auto_now_add=True)
+    start_at = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.name
