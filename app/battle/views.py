044622595637de57ba62d205a66acbfc2ff478a3
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-10-03 16:59:56 +0300

    Added static!

--- app/battle/views.py
+++ app/battle/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+
+class HomeView(View):
+    """Домашняя страница"""
+    def get(self, request, *args, **kwargs):
+        return render(request, 'battle/home.html')
--- app/userprofile/models.py
+++ app/userprofile/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """Профиль пользователя. Расширяет модель User"""
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.user.name
