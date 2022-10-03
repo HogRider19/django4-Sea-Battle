404e5c433107992d6b1747b64d8fd9c20e57bdf6
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-10-03 15:13:55 +0300

    Added layout!

--- app/battle/admin.py
+++ app/battle/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['name', 'player1', 'player2', 'start_at', 'finished_at']
+
+
+admin.site.register(Game, GameAdmin)
 
--- app/battle/models.py
+++ app/battle/models.py
@@ -5,18 +5,22 @@ from django.contrib.auth.models import User
 class Game(models.Model):
     """Модель игры"""
     name = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=10)
-    battle_map = models.CharField(max_length=500)
-    winner = models.OneToOneField(User, on_delete=models.DO_NOTHING)
-    player1 = models.OneToOneField(
-        User, related_name='game', on_delete=models.DO_NOTHING)
-    player2 = models.OneToOneField(
-        User, related_name='game', on_delete=models.DO_NOTHING)
-    spectators = models.ManyToManyField(User, related_name='viewed_game')
-    subscribers = models.ManyToManyField(User, related_name='viewed_game')
+    password = models.CharField(max_length=10, blank=True, null=True)
+    battle_map = models.CharField(max_length=500, blank=True, null=True)
+    step_counter = models.PositiveIntegerField(default=0)
+    winner = models.OneToOneField(
+        User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    player1 = models.ForeignKey(
+        User, related_name='game_player1', on_delete=models.DO_NOTHING)
+    player2 = models.ForeignKey(
+        User, related_name='game_player2', on_delete=models.DO_NOTHING)
+    spectators = models.ManyToManyField(
+        User, related_name='viewed_game', blank=True)
+    subscribers = models.ManyToManyField(
+        User, related_name='signed_game', blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
-    start_at = models.DateTimeField()
-    finished_at = models.DecimalField()
+    start_at = models.DateTimeField(blank=True, null=True)
+    finished_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
