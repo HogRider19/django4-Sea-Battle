7a6ff248d2a7c9f8682772f5afb1148b2c748c54
HogRider <98631205+HogRider19@users.noreply.github.com>
2022-10-05 19:32:02 +0300

    GameList!

--- app/battle/urls.py
+++ app/battle/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('gamelist/', views.GameListView.as_view(), name='gamelist'),
     path('', views.HomeView.as_view(), name='home'),
 ]
--- app/battle/views.py
+++ app/battle/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View
+from .models import Game
+from django.views.generic import View, ListView
 
 
 
@@ -7,3 +8,12 @@ class HomeView(View):
     """Домашняя страница"""
     def get(self, request, *args, **kwargs):
         return render(request, 'battle/home.html')
+
+
+class GameListView(ListView):
+    model = Game
+    template_name = 'battle/gamelist.html'
+    context_object_name = 'games'
+
+    def get_queryset(self):
+        return Game.objects.all()
